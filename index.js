5561b8322bb6790ae1c529afbb36d61d5e2b3384
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-05-26 23:49:59 +0600

    added get method to get job and specific job by id

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +10,52 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// uri
+const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.qtoljre.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const dataBase = client.db("careerCode");
+    const jobCollection = dataBase.collection("jobs");
+    // get jobs by get method
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // specific job by their id
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+
+      const result = await jobCollection.findOne(query);
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("career code server");
