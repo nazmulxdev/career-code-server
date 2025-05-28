8dea7755b30720cc1916369a877010e46230afdd
Md Nazmul Hossen <nazmulhossen.hstu.21@gmail.com>
2025-05-29 02:33:30 +0600

    added job application route for getting total number of job applied for the speific job

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { application } from "express";
 import cors from "cors";
 import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 import "dotenv/config";
@@ -29,11 +29,44 @@ async function run() {
 
     const dataBase = client.db("careerCode");
     const jobCollection = dataBase.collection("jobs");
+    const applicationCollection = dataBase.collection("applications");
     // get jobs by get method
+
     app.get("/jobs", async (req, res) => {
-      const cursor = jobCollection.find();
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.hr_email = email;
+      }
+
+      const cursor = jobCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
+    });
+
+    //post jobs by post method
+    app.post("/jobs", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobCollection.insertOne(newJob);
+      res.send(result);
+    });
+
+    app.get("/jobs/applications", async (req, res) => {
+      const email = req.query.email;
+      const query = { hr_email: email };
+      const jobs = await jobCollection.find(query).toArray();
+
+      // should use aggregate to have optimum data fetching
+      for (const job of jobs) {
+        const applicationQuery = {
+          jobId: job._id.toString(),
+        };
+        const application_count = await applicationCollection.countDocuments(
+          applicationQuery
+        );
+        job.application_count = application_count;
+      }
+      res.send(jobs);
     });
 
     // specific job by their id
@@ -47,6 +80,75 @@ async function run() {
       const result = await jobCollection.findOne(query);
       res.send(result);
     });
+
+    // posting applicant details in the job details
+
+    app.post("/applications", async (req, res) => {
+      const applicationDetails = req.body;
+      console.log(applicationDetails);
+      const result = await applicationCollection.insertOne(applicationDetails);
+      res.send(result);
+    });
+
+    app.patch("/applications/:id", async (req, res) => {
+      const id = req.params.id;
+      const update = req.body;
+      const filter = {
+        _id: new ObjectId(id),
+      };
+      const updatedDoc = {
+        $set: {
+          status: update.status,
+        },
+      };
+      const result = await applicationCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.get("/applications", async (req, res) => {
+      const result = await applicationCollection.find().toArray();
+      res.send(result);
+    });
+
+    // application api by email and uid query
+
+    app.get("/application", async (req, res) => {
+      const applicantUID = req.query.applicantUID;
+      const applicantEmail = req.query.applicantEmail;
+      const query = {
+        applicantUID,
+        applicantEmail,
+      };
+      const result = await applicationCollection.find(query).toArray();
+      //   bad way to aggregate data
+      for (const application of result) {
+        const jobId = application.jobId;
+        const jobQuery = {
+          _id: new ObjectId(jobId),
+        };
+        const job = await jobCollection.findOne(jobQuery);
+        application.company = job.company;
+        application.title = job.title;
+        application.company_logo = job.company_logo;
+      }
+      console.log(result);
+      res.send(result);
+    });
+
+    //
+
+    app.get("/applications/job/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = {
+        jobId: id,
+      };
+
+      const result = await applicationCollection.find(query).toArray();
+      console.log(result);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
